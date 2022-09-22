c627691eb084b4b4bfb6928229afc6557af60ec8
4rip22 <112571332+4rip22@users.noreply.github.com>
2022-09-22 17:16:45 +0700

    Add files via upload

---
+++ arip saputra_2217051061_praktest3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+int angka;
+cin >> angka;
+
+if(angka==1){
+	cout<<"satu\n";
+}
+else if(angka==2){
+	cout<<"dua\n";
+}
+else if(angka==3){
+	cout<<"tiga\n";
+}
+
+else
+{
+	cout << "bilangan lebih dari tiga\n";
+}
+
+
+ 
+	
+	
+}
+
